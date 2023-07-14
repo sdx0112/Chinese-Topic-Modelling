a2dc659ad38bde33311832b7fc33cb0e5ad28765
Dongxu <sdx0112@gmail.com>
2023-07-14 18:09:33 +0800

    Remove empty files

--- GPT-FewShot.py
+++
@@ -1,6 +0,0 @@
-"""
-Use small manually-labeled set as examples and try few-shot of GPT model.
-"""
-
-from langchain.prompts.few_shot import FewShotPromptTemplate
-from langchain.prompts.prompt import PromptTemplate
